57745291fa064eb5a13cb535fc5999a4abdeaea4
ashwanisng <ashwanicena5@gmail.com>
2021-09-20 19:33:21 +0530

    .

--- lib/app/modules/entry/views/entry_view.dart
+++ lib/app/modules/entry/views/entry_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:splash_screen_view/SplashScreenView.dart';
 import 'package:your_coin/app/enviroment/utils/env.dart';
 
 import 'package:your_coin/app/modules/entry/controllers/entry_controller.dart';
-import 'package:your_coin/app/modules/entry/views/pages/auth_view.dart';
 
 class EntryView extends GetView<EntryController> {
   @override
